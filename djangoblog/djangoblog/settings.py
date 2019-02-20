f08743ebee6d9268c0596e604e79af433808aad3
24gaurangi <gwanjari19@gmail.com>
2019-02-19 20:53:01 -0800

    Updated Procfile

--- djangoblog/djangoblog/settings.py
+++ djangoblog/djangoblog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bloglist',
     'useraccounts',
+    'djangoblog',
 ]
 
 MIDDLEWARE = [
@@ -132,4 +133,4 @@ MEDIA_ROOT = (
     os.path.join(BASE_DIR, 'media')
 )
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
