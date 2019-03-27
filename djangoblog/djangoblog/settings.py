6323c3e07c8f23bda0aadc6a17413796d00625b6
24gaurangi <gwanjari19@gmail.com>
2019-03-27 16:19:59 -0700

    static file updated

--- djangoblog/djangoblog/settings.py
+++ djangoblog/djangoblog/settings.py
@@ -14,7 +14,8 @@ import os
 #import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,8 +121,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
